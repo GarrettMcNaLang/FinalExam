0138901bfd7b6d5545fa7796f886715cc115a7c1
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2022-12-07 00:24:13 -0500

    start/pausemenu

--- New Unity Project/Assets/Scripts/PauseMenu.cs
+++ New Unity Project/Assets/Scripts/PauseMenu.cs
@@ -1,48 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject menu;
-    public bool paused = false;
-    
-    // Start is called before the first frame update
+    public GameObject pausemenu;
+    public static bool ispaused;
+
     void Start()
     {
-        menu.SetActive(false);
+        pausemenu.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-            {
-
-            if (Input.GetButtonDown("cancel"))
-            {
-                TogglePauseGame();
-            }
-        }
-    }
-    public void TogglePauseGame()
-    {
-        paused = !paused;
-
-        menu.SetActive(paused);
-
-        if(paused)
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
-            Time.timeScale = 0f;
+            PauseGame();
         }
         else
         {
-            Time.timeScale = 1f;
+            ResumeGame();
         }
     }
+
+    public void PauseGame()
+    {
+        pausemenu.SetActive(true);
+        Time.timeScale = 0f;
+        ispaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pausemenu.SetActive(false);
+        Time.timeScale = 1f;
+        ispaused = false;
+    }
+
     public void ExitGame()
     {
-        Application.Quit();
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("StartMenu");
     }
-   
 }
