c09db11f7aa78a49515df0eae025d06043d3ddaa
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2023-01-09 23:38:08 -0500

    main

--- New Unity Project/Assets/Scripts/TeleportScript.cs
+++ New Unity Project/Assets/Scripts/TeleportScript.cs
@@ -1,31 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TeleportScript : MonoBehaviour
 {
-
+    
+    public Sprite Detective;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Bus"))
         {
-            Debug.Log("YES");
+           // Debug.Log("YES");
             transform.position = new Vector3(60, -18, -1);
         }
 
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            Debug.Log("YES");
+            //Debug.Log("YES");
             transform.position = new Vector3(-60, -40, -1);
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Detective;
         }
 
         if (collision.gameObject.CompareTag("Door"))
         {
-            Debug.Log("YES");
+            //Debug.Log("YES");
             transform.position = new Vector3(10, -60, -1);
+
+        }
+        if (collision.gameObject.CompareTag("Closet"))
+            {
+            Debug.Log("Yes");
+
+            SceneManager.LoadScene("StartMenu");
+            }
+           
         }
     }
 
 
 
-}
